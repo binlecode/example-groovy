52d80d49a84fed28e482a23642bc45df58718c57
Bin Le <bin.le.code@gmail.com>
2019-05-17 21:36:00 -0400

    add actor error control wip

--- src/concurrent/eg_gpars_actor_error_control.groovy
+++ src/concurrent/eg_gpars_actor_error_control.groovy
@@ -1,6 +1,8 @@
 @Grab(group='org.codehaus.gpars', module='gpars', version='1.2.1')
 
 import groovyx.gpars.actor.Actors
+import groovyx.gpars.actor.DefaultActor
+import groovyx.gpars.actor.DynamicDispatchActor
 
 /**
  * Instead of using try-catch to wrap logic block, an {@code onException()} method can be defined
@@ -25,5 +27,72 @@ act.join()
  * But to keep actor continue working with error, we need Supervising that supports:
  * - Get notified of changes on other actors
  * - Take control over them (stopping, restarting…etc)
+ * GPars doesn't have have Supervisor type, but we can create one easily with its
+ * {@link groovyx.gpars.actor.DynamicDispatchActor} class.
  */
+class Supervisor extends DynamicDispatchActor {
 
+    Supervised supervised
+    Class<? extends Supervised> supervisedClass
+
+    void link(Supervised supervised) {
+        this.supervised = supervised
+        this.supervised.supervisor = this
+        this.supervisedClass = supervised.class
+    }
+}
+
+/**
+ * Supervised actor will send the exception message to supervisor
+ */
+class Supervised extends DefaultActor {
+    Supervisor supervisor
+
+    // add onException callback to send error message to supervisor
+    void onException(Throwable th) {
+        this.supervisor << new SupervisedException(th.message)
+    }
+}
+
+// now let's create a supervised actor
+class SimpleSupervisedActor extends Supervised {
+    void act() {
+        loop {
+            react { String msg ->
+                if (msg == 'disconnected') {
+                    throw new SupervisedException('no! this is not true')
+                } else {
+                    println "ok, we are connected with: $msg"
+                }
+            }
+        }
+    }
+}
+
+class SupervisedException extends RuntimeException {
+    SupervisedException(String msg) {
+        super(msg)
+    }
+}
+
+def ssa = new SimpleSupervisedActor().start()
+
+def spv = new Supervisor() {
+    void onMessage(String message) {
+        supervised << message
+    }
+
+    void onMessage(SupervisedException se) {
+        println "SUPERVISED ACTOR DIES WITH MESSAGE: ${se.message}"
+        supervised = supervisedClass.newInstance().start()
+    }
+}.start()
+
+spv.link(ssa)
+
+spv << 'sky'
+spv << 'ocean'
+spv << 'disconnected'
+spv << 'garden'
+
+[spv, ssa]*.join()
