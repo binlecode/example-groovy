cdd68f0b7197968bfd8644965500b89d6f0597e4
Bin Le <bin.le.code@gmail.com>
2019-05-23 23:43:30 -0400

    modify gpars threadpool simple examples

--- src/concurrent/eg_gpars_threadpool.groovy
+++ src/concurrent/eg_gpars_threadpool.groovy
@@ -1,9 +1,22 @@
 @Grab(group = 'org.codehaus.gpars', module = 'gpars', version = '1.2.1')
 import groovyx.gpars.GParsExecutorsPool
+import groovyx.gpars.GParsPool
+
+
+// Quick and dirty: .withPool() provides a thread pool as an instance of ForkJoinPool
+// The withPool method returns only after all the worker threads have finished their tasks
+// and the pool has been destroyed, returning the resulting value of the associated block of code.
+GParsPool.withPool {   //
+    // gpars parallel array processing preserves source array's original order
+    println((1..100).findAllParallel {
+        it > 40
+    }.collectParallel { it * 2 })   // gpars provides a lot of xxxParallel methods
+}
+
 
 def longRunningMethod(name) {
     println "hello $name, running ..."
-    Thread.sleep(3000)
+    Thread.sleep(1000)
 }
 
 // customize thread pool size
@@ -13,10 +26,16 @@ GParsExecutorsPool.withPool(2) {
     }
 
     ['a', 'b', 'c', 'd', 'e'].each {
-
         longRunningMethodWrapper.callAsync(it)
     }
 }
 
+// we can also import static withPool method
+import static groovyx.gpars.GParsPool.withPool
 
+def rnd = new Random(new Date().time)
+withPool {
+    def cnt = (1..100).collect {rnd.nextInt(99)}.countParallel { it > 50 }
+    println "how many above 50: $cnt"
+}
 
